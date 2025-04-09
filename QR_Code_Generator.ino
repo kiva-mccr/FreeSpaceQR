88f8c033311030765c1f9c187eb2ff194e943ad3
SophiaCodesA <Sophiaadams1103@gmail.com>
2025-04-09 15:32:33 -0400

    Sped it up alil, added white screen after message.

--- QR_Code_Generator.ino
+++ QR_Code_Generator.ino
@@ -93,7 +93,10 @@ void loop() {
             }
         }
     }
-    delay(2000);
+    delay(700);
   }
   
+    tft.fillScreen(WHITE); //wipe the screen first
 } 
+
+// Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.
